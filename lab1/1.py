f3964b65eebe3b74cb469282435ca78bea5b1dac
Aidark111 <sansyzbaidarkhan@gmail.com>
2024-01-25 17:37:29 +0600

    111

--- lab1/1.py
+++ lab1/1.py
@@ -1,1 +1,5 @@
-print("Hello World!!")
+print("Hello World!!") 
+##
+if 5 > 2:
+     print("YES")
+     
---
+++ lab1/3.py
@@ -0,0 +1,23 @@
+carname = "volvo"
+##
+x=50
+y = 10
+print(x+y)
+
+##
+
+x = 5
+y = 10
+z = x + y
+print(z)
+###
+x.y.z = "Orange", "Banana", "Cherry"
+
+##
+x = y = z = "Orange"
+
+##
+
+def myfunc():
+    global x
+    x = "fantastic"
