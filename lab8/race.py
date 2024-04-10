2ecf1b4047f96e87f033626e2036a3a024ba1ff5
Aidark111 <sansyzbaidarkhan@gmail.com>
2024-04-11 04:05:39 +0500

    lab89

---
+++ lab8/race.py
@@ -0,0 +1,122 @@
+import pygame
+import sys
+import random
+from pygame.locals import *
+
+pygame.init()
+
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+DISPLAYSURF = pygame.display.set_mode((400, 600))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
+
+player_img = pygame.image.load("/Users/sansyzbaidarkhan/Desktop/pp2/lab8/png/player.png")
+enemy_img = pygame.image.load("/Users/sansyzbaidarkhan/Desktop/pp2/lab8/png/enemy.png")
+coin_img = pygame.image.load("/Users/sansyzbaidarkhan/Desktop/pp2/lab8/png/Coin.png")
+
+scaled_player = pygame.transform.scale(player_img, (45, 60))
+scaled_enemy = pygame.transform.scale(enemy_img, (45, 60))
+scaled_coin = pygame.transform.scale(coin_img, (35, 35))
+
+font = pygame.font.Font(None, 36)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = scaled_enemy
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        self.rect.move_ip(0, 10)
+        if self.rect.bottom > 600:
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = scaled_player
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = scaled_coin
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), -50)
+
+    def move(self):
+        self.rect.move_ip(0, 10)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.bottom = 0
+            self.rect.center = (random.randint(30, 370), -50)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+player = Player()
+enemy = Enemy()
+coin = Coin()
+
+coins_collected = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    player.update()
+    enemy.move()
+    coin.move()
+
+    if pygame.sprite.collide_rect(player, coin):
+        coins_collected += 1
+        coin.rect.center = (random.randint(40, SCREEN_WIDTH - 40), -50)
+
+    if pygame.sprite.collide_rect(player, enemy):
+        print("Game Over")  
+        pygame.quit()
+        sys.exit()
+
+    DISPLAYSURF.fill(WHITE)
+    player.draw(DISPLAYSURF)
+    enemy.draw(DISPLAYSURF)
+    coin.draw(DISPLAYSURF)
+
+    
+    coin_text = font.render("Coins: " + str(coins_collected), True, BLACK)
+    DISPLAYSURF.blit(coin_text, (SCREEN_WIDTH - 150, 10))
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
